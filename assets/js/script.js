f371ccc8e551fecbb37a35240dccae5054900e1b
Aliaksandr Ziaziulchyk <sanazez1234@gmail.com>
2025-05-29 17:44:45 +0300

    added mobile menu and second block in main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,10 +36,44 @@ const init = () => {
                 prevEl: '.main__about-button-prev',
             },
             breakpoints: {
-                920: {
+                1280: {
                     slidesPerView: 3,
                 },
+                848: {
+                    slidesPerView: 2,
+                },
+                320: {
+                    slidesPerView: 1,
+                },
             },
+            pagination: {
+                el: '.main__about-pagination',
+                clickable: true,
+                dynamicBullets: true,
+                bulletClass: 'main__about-dot',
+                bulletActiveClass: 'main__about-dot--active',
+            },
+        });
+    });
+
+    const burgerButton = document.querySelector('.header__burger');
+    const closeButton = document.querySelector('.header__close');
+    const mobMenu = document.querySelector('.header__mobile-menu');
+    const menuLinks = document.querySelectorAll('.header__menu a');
+
+    burgerButton.addEventListener('click', () => {
+        mobMenu.classList.add('active');
+        burgerButton.classList.add('hidden');
+    });
+
+    closeButton.addEventListener('click', () => {
+        mobMenu.classList.remove('active');
+        burgerButton.classList.remove('hidden');
+    });
+
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            mobMenu.classList.remove('active');
         });
     });
 };
